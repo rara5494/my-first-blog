5f0d69e84d6623a0cdf8066cff0e6d3c8f20c678
unknown <indiradaranindra94@gmail.com>
2018-11-24 13:50:52 +0100

    add comment section and everything else

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,11 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf.urls import include, url
 from django.urls import path, include
 from django.contrib import admin
 
+from django.contrib.auth import views
+
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
